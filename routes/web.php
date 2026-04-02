290b33c893382975a460e6b7e079dcdce94c33f0
Stefani Parera <angelinaparera22-del.com>
2026-04-02 09:01:46 +0800

    Pertemuan 2: Blade Template

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome',);
+});
+
+Route::get('/student', function () {
+    return view('student.index', ['title' => 'STUDENT']);
+});
+
+Route::get('/student/create', function () {
+    return view('student.create', ['title' => 'CREATE STUDENT']);
 });
