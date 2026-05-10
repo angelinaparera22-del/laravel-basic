54eb4b91474739b61d5eff02f0b9e5a3a5eced53
Stefani Parera <angelinaparera22-del.com>
2026-05-11 02:06:51 +0800

    Pertemuan 9: QueryScope & One To One

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -6,19 +6,32 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 
 #[Fillable(['name', 'department_id'])]
 
 class Lecturer extends Model
 {
-    /** @use HasFactory<\Database\Factories\LecturerFactory> */
     use HasFactory;
 
     protected $with = ['department'];
-     
+
     public function department(): BelongsTo
     {
-        return $this->belongsTo(department::class);
+        return $this->belongsTo(Department::class);
+    }
+
+    #[Scope]
+    protected function filter(Builder $query, array $filters): void
+    {
+        $query
+            ->when($filters['keyword'] ?? false, function ($query, $keyword) {
+                return $query->where('name', 'like', '%' . $keyword . '%');
+            })
+
+            ->when($filters['department_id'] ?? false, function ($query, $department) {
+                return $query->where('department_id', $department);
+            });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +11,10 @@ Route::get('/', [StudentController::class, 'index']);
 Route::get('/Student', [StudentController::class, 'index'])->name('Student.index');
 Route::get('/Student/create', [StudentController::class, 'create'])->name('Student.create');
 Route::post('/Student/store', [StudentController::class, 'store'])->name('Student.store');
-Route::get('/Student/{student}edit', [StudentController::class, 'edit'])->name('Student.edit');
+Route::get('/Student/{student}/edit', [StudentController::class, 'edit'])->name('Student.edit');
 Route::put('/Student/{student}', [StudentController::class, 'update'])->name('Student.update');
 Route::delete('/Student/{student}', [StudentController::class, 'destroy'])->name('Student.destroy');
 
 Route::resource('department', DepartmentController::class);
 Route::resource('lecturer', LecturerController::class);
+Route::resource('organization', OrganizationController::class);
